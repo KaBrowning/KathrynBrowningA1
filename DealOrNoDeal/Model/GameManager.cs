8745603c860bca33802ce257782e042bc535670b
Kathryn Browning <kbrowni3@my.westga.edu>
2016-09-04 09:39:42 -0400

    Removed the duplicate list values for dollarValuesForGame and dollarValuesInPlay

--- DealOrNoDeal/Model/GameManager.cs
+++ DealOrNoDeal/Model/GameManager.cs
@@ -47,7 +47,7 @@ namespace DealOrNoDeal.Model
         {
             this.dollarValuesForGame = new List<int> { 0, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000 };
             this.briefCaseDollarValues = new List<int>();
-            this.dollarValuesInPlay = new List<int> { 0, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000 };
+            this.dollarValuesInPlay = new List<int>(this.dollarValuesForGame);
             this.banker = new Banker();
             this.allBankOffers = new List<int>();
 
